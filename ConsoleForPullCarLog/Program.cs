0ea472d5a58968dd9f5e92d5bfe98c0ceff753f6
pengwei <pengweiaini123@163.com>
2024-07-13 08:55:17 +0800

    console应用程序修改

--- ConsoleForPullCarLog/Program.cs
+++ ConsoleForPullCarLog/Program.cs
@@ -1,13 +1,27 @@
-﻿namespace ConsoleForPullCarLog
+﻿using ConsoleForPullCarLog.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ConsoleForPullCarLog
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Thread.CurrentThread.Name = "Main";
-            Console.WriteLine(String.Format("Thread-{0}:Connect ADB",Thread.CurrentThread.Name));
-            Console.WriteLine("adb devices");
-            Console.ReadKey();
+            var host = CreateHostBuilder(args).Build();
+            var worker = ActivatorUtilities.CreateInstance<Work>(host.Services);
+            worker.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, configuration) => { 
+                    configuration.Sources.Clear();
+                    configuration.AddJsonFile(@"Config/AppSettings.json", optional: true, reloadOnChange: true);
+                    configuration.AddCommandLine(args);
+                });
         }
     }
 }
---
+++ ConsoleForPullCarLog/Services/Work.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleForPullCarLog.Services
+{
+    public class Work
+    {
+        private readonly IConfiguration configuration;
+        private readonly ILogger<Work> logger;
+
+
+        public Work(IConfiguration configuration, ILogger<Work> logger)
+        {
+            this.configuration = configuration;
+            this.logger = logger;
+        }
+
+        public void Run()
+        {
+            logger.LogInformation("hello, world!!!");
+            var timeDump = configuration.GetConnectionString("TimeDump");
+            Trace.WriteLine(timeDump);
+            logger.LogInformation($"Pull Log During The {timeDump}");
+            logger.LogInformation(configuration["key1"]);
+            Console.ReadKey();
+        }
+
+    }
+}
