b849edb88eb044b9777e86da7be6e82aad19b1e6
AutoTest_2 <haiyang.zheng@thundersoft.com>
2024-08-28 16:52:11 +0800

    线程demo添加

--- WpfForThreadDemo/MainWindow.xaml.cs
+++ WpfForThreadDemo/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +25,32 @@ namespace WpfForThreadDemo
         public MainWindow()
         {
             InitializeComponent();
+            ThreadDemo.Click += ThreadDemo_Click;
+        }
+
+        private void ThreadDemo_Click(object sender, RoutedEventArgs e)
+        {
+            //使用Thread类来异步编程
+            Thread thread = new Thread(new ThreadStart(() =>
+            {
+                Thread.Sleep(5000);
+                MessageBox.Show("time is over");
+            }));
+            
+            thread.Start();
+            //通过async和await关键字
+            //ToDoWorking();
+
+        }
+
+        public async void ToDoWorking()
+        {
+            await Task.Run(() => {
+                Thread.Sleep(8000);
+                MessageBox.Show("show by async");
+                Thread.Sleep(8000);
+            }); 
+           
         }
     }
 }
