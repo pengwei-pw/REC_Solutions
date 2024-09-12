7e7ae2498e0fd6c98bc9e03f5a496c8cb2742ff7
AutoTest_2 <haiyang.zheng@thundersoft.com>
2024-09-12 10:32:07 +0800

    threadpool and task

--- WpfForThreadDemo/MainWindow.xaml.cs
+++ WpfForThreadDemo/MainWindow.xaml.cs
@@ -31,26 +31,47 @@ namespace WpfForThreadDemo
         private void ThreadDemo_Click(object sender, RoutedEventArgs e)
         {
             //使用Thread类来异步编程
-            Thread thread = new Thread(new ThreadStart(() =>
-            {
-                Thread.Sleep(5000);
-                MessageBox.Show("time is over");
-            }));
-            
-            thread.Start();
+            //Thread thread = new Thread(new ThreadStart(() =>
+            //{
+            //    Thread.Sleep(5000);
+            //    MessageBox.Show("time is over");
+            //}));
+
+            //thread.Start();
             //通过async和await关键字
-            //ToDoWorking();
-
+            ToDoWorking();
+            //通过线程池
+            //ThreadPool.SetMaxThreads(10, 5);
+            //WaitCallback waitCallback = new WaitCallback((obj) => {
+            //    int? str = (int?)obj;
+            //    Console.WriteLine(str);
+            //    Trace.WriteLine(str);
+            //});
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    ThreadPool.QueueUserWorkItem(waitCallback, i);
+            //}
+            //Parallel class
+            //Parallel.Invoke();
         }
-
+        public async Task<String> doSomeConsumeTimeAsync()
+        {
+            await Task.Delay(5000);
+            return "i am ok";
+        } 
+        public void DoJ()
+        {
+            //Thread.Sleep(2000);
+            MessageBox.Show("doj ok");
+        }
         public async void ToDoWorking()
         {
-            await Task.Run(() => {
-                Thread.Sleep(8000);
-                MessageBox.Show("show by async");
-                Thread.Sleep(8000);
-            }); 
-           
+            Task<string> cc = doSomeConsumeTimeAsync();
+            
+            DoJ();
+
+            string value = await cc;
+            MessageBox.Show(value);
         }
     }
 }
